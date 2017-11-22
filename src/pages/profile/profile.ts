a4b778fd93ba6486f774b3641ac913998092d91c
payen83 <payen83@gmail.com>
2017-11-22 10:42:51 +0800

    Profile Page
    - Add profile
    - save profile
    - age
    - validation

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -14,12 +14,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-
+  public person: {name: string, company: string, birthdate?: number};
+  dob: any;
+  age: any;
+  showProfile: boolean;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.person = {name: undefined, company: undefined, birthdate: undefined};
+    this.dob = undefined;
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
+    let person = JSON.parse(localStorage.getItem('PERSON'));
+    if (person){
+      this.person = person;
+      this.age = this.getAge(this.person.birthdate);
+      this.dob = new Date(this.person.birthdate).toISOString();
+    }
   }
+
+  reset(){
+    this.person = {name: null, company: null, birthdate: null};
+    this.dob = null;
+    this.showProfile = false;
+  }
+
+  save(){
+    this.person.birthdate = new Date(this.dob).getTime();
+    this.age = this.getAge(this.person.birthdate);
+    this.showProfile = true;
+    localStorage.setItem('PERSON', JSON.stringify(this.person));
+  }
+
+  getAge(birthdate){
+    let currentTime = new Date().getTime();
+     return ((currentTime - birthdate)/31556952000).toFixed(0);
+  }
+
 
 }
